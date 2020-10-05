e33705fdd983bd69c4a3353fbd47a9e403224902
kitty.cheng <450038893@qq.com>
2020-10-06 06:00:18 +0800

    增加路由导航守卫

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,40 +1,37 @@
 <template>
-    <div class="login_container">
-        <div class="login_box">
-            <div class="avatar_box">
-                <img src="../assets/logo.png" alt="" />
-            </div>
-            <!-- form 表单 -->
-            <el-form
-                ref="loginFormRef"
-                :model="loginForm"
-                :rules="rules"
-                label-width="0px"
-                class="login_form"
-            >
-                <!-- 用户名 -->
-                <el-form-item prop="username">
-                    <el-input
-                        prefix-icon="iconfont icon-user"
-                        v-model="loginForm.username"
-                    ></el-input>
-                </el-form-item>
-                <!-- 密码 -->
-                <el-form-item prop="password">
-                    <el-input
-                        prefix-icon="iconfont icon-3702mima"
-                        v-model="loginForm.password"
-                        type="password"
-                    ></el-input>
-                </el-form-item>
-                <!-- 按钮区域 -->
-                <el-form-item class="btns">
-                    <el-button type="primary" size="default">登录</el-button>
-                    <el-button type="info" size="default" @click="resetLoginForm">重置</el-button>
-                </el-form-item>
-            </el-form>
-        </div>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- form 表单 -->
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="rules"
+        label-width="0px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-user" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="iconfont icon-3702mima"
+            v-model="loginForm.password"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" size="default" @click="login">登录</el-button>
+          <el-button type="info" size="default" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
@@ -42,8 +39,8 @@ export default {
   data () {
     return {
       loginForm: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
       },
       rules: {
         username: [
@@ -62,6 +59,19 @@ export default {
     resetLoginForm () {
       // console.log(this)
       this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+
+        // 登录成功写入token到sessionStorage中，并跳转
+        window.sessionStorage.setItem('token', res.data.token)
+        // 通过编程式导航跳转到后台主页 路由地址是 /home
+        this.$router.push('/home')
+      })
     }
   }
 }
@@ -69,50 +79,50 @@ export default {
 
 <style lang="less" scoped>
 .login_container {
-    background-color: #2b4b6b;
-    height: 100%;
+  background-color: #2b4b6b;
+  height: 100%;
 }
 .login_box {
-    width: 450px;
-    height: 300px;
-    background-color: #fff;
-    border-radius: 3px;
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
     box-shadow: 0 0 10px #ddd;
     position: absolute;
     left: 50%;
-    top: 50%;
     transform: translate(-50%, -50%);
+    background-color: #fff;
 
-    .avatar_box {
-        width: 130px;
-        height: 130px;
-        border: 1px solid #eee;
-        border-radius: 50%;
-        padding: 10px;
-        box-shadow: 0 0 10px #ddd;
-        position: absolute;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background-color: #fff;
-
-        img {
-            width: 100%;
-            height: 100%;
-            border-radius: 50%;
-            background-color: #eee;
-        }
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
     }
+  }
 }
 
 .login_form {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
 .btns {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -19,4 +19,17 @@ const router = new Router({
   routes
 })
 
+// 挂在路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //  next() 放行   next('login') 强制跳转
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokensStr = window.sessionStorage.getItem('token')
+  if (!tokensStr) return next('/login')
+  next()
+})
+
 export default router
