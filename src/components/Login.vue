8c11d3bfe713e93bac929954dcb3d454fea01e34
kitty.cheng <450038893@qq.com>
2020-10-05 22:08:13 +0800

    init vue project

---
+++ src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login-container">登录组件</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  background-color: #2b4b6b;
+}
+</style>
