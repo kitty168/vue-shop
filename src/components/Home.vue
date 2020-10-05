72357c65be9d765211d83470cc701d8b52b42ebf
kitty.cheng <450038893@qq.com>
2020-10-06 06:09:41 +0800

    finish logout

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    后台首页
+    <el-button type="primary" size="default" @click="logout">退出登录</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
