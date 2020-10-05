3b066966ab83532a6e0cd005b1ea8d9f1fadc430
kitty.cheng <450038893@qq.com>
2020-10-06 07:03:45 +0800

    增加container布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
-    后台首页
-    <el-button type="primary" size="default" @click="logout">退出登录</el-button>
-  </div>
+  <el-container class="home_container">
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" alt="" width="45" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" size="default" @click="logout">退出登录</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -17,4 +26,29 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home_container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 5px;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      padding-left: 10px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
