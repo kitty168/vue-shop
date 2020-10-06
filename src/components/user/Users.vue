a39aeb8779e2c1415f5054f187695162ca119480
kitty.cheng <450038893@qq.com>
2020-10-06 09:51:41 +0800

    添加面包屑和card及搜索区

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,3 +1,34 @@
 <template>
-  <div>用户列表组件</div>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片区域 -->
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="username" class="input-with-select">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" size="default">添加</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: 'zs'
+    }
+  }
+}
+</script>
