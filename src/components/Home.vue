f254d708921cef4dadb6fcb1bf513f42c66a33e7
kitty.cheng <450038893@qq.com>
2020-10-06 08:37:23 +0800

    菜单折叠效果

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,28 +11,42 @@
     <!-- 页面主体区域 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width="220px">
+      <el-aside :width="isCollapse ? '64px' : '220px'">
+        <div class="toggle-button" @click="toggleCollapse">
+          <i :class="isCollapse ? 'el-icon-s-unfold' : 'el-icon-s-fold'"></i>
+        </div>
         <!-- 侧边栏菜单导航区域 -->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color=""
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+        >
           <!-- 一级菜单 -->
-          <el-submenu index="1">
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
             <!-- 一级菜单模板区 -->
             <template slot="title">
               <!-- 图标 -->
-              <i class="el-icon-location"></i>
+              <i :class="iconfonts[item.id]"></i>
               <!-- 文本 -->
-              <span>导航一</span>
+              <span>{{ item.authName }}</span>
             </template>
 
             <!-- 二级菜单 -->
-            <el-menu-item index="1-1"
-              ><template slot="title">
-                <!-- 图标 -->
-                <i class="el-icon-location"></i>
-                <!-- 文本 -->
-                <span>导航2</span>
-              </template></el-menu-item
+            <el-menu-item
+              :index="subItem.id + ''"
+              v-for="subItem in item.children"
+              :key="subItem.id"
             >
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
           </el-submenu>
         </el-menu>
       </el-aside>
@@ -44,10 +58,39 @@
 
 <script>
 export default {
+  data () {
+    return {
+      // 左侧菜单数据
+      menuList: [],
+      iconfonts: {
+        125: 'iconfont icon-users',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      // 左侧菜单是否折叠
+      isCollapse: false
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    // 获取菜单数据
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+      console.log(this.menuList)
+    },
+    // 点击按钮，切换侧栏菜单的折叠与展开
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
@@ -75,8 +118,24 @@ export default {
 }
 .el-aside {
   background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
 }
 .el-main {
   background-color: #eaedf1;
+}
+.iconfont {
+  padding-right: 5px;
+}
+
+// 折叠侧栏
+.toggle-button {
+  background-color: #4a5064;
+  color: #fff;
+  text-align: right;
+  padding-right: 10px;
+  font-size: 16px;
+  line-height: 24px;
 }
 </style>
