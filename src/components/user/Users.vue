29d445159a1faa7945d3ff74f7099cbd9129ce1f
kitty.cheng <450038893@qq.com>
2020-10-07 16:39:43 +0800

    add el-table and el-pagination

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -11,7 +11,7 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容" v-model="username" class="input-with-select">
+          <el-input placeholder="请输入内容" class="input-with-select">
             <el-button slot="append" icon="el-icon-search"></el-button>
           </el-input>
         </el-col>
@@ -19,6 +19,43 @@
           <el-button type="primary" size="default">添加</el-button>
         </el-col>
       </el-row>
+
+      <!-- datatable列表区域 -->
+      <el-table :data="userList" border stripe>
+        <el-table-column label="序号" type="index" width="50"></el-table-column>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <!-- 编辑按钮 -->
+            <el-button type="primary" icon="el-icon-edit" size="mini" circle></el-button>
+            <!-- 删除按钮 -->
+            <el-button type="danger" icon="el-icon-delete" size="mini" circle></el-button>
+            <!-- 分配权限 -->
+            <el-tooltip effect="dark" content="分配权限" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini" circle></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -27,7 +64,37 @@
 export default {
   data () {
     return {
-      username: 'zs'
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      },
+      userList: [],
+      total: 0
+    }
+  },
+  created () {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList () {
+      const { data: res } = await this.$http.get('users', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) return this.$message.error('获取用户列表失败')
+      this.userList = res.data.users
+      this.total = res.data.total
+      // console.log(res)
+    },
+    // 监听 pagesize 改变得事件
+    handleSizeChange (pagesize) {
+      this.queryInfo.pagesize = pagesize
+      this.getUserList()
+    },
+    // 监听 页码值 改变得事件
+    handleCurrentChange (pagenum) {
+      this.queryInfo.pagenum = pagenum
+      this.getUserList()
     }
   }
 }
